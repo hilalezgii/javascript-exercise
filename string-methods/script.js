71773476d7b595e6622fe41bb98a607f6ca9f361
hilalezgii <hilal_ezgi_2003@hotmail.com>
2023-12-15 01:56:31 +0300

    feat: string methods

---
+++ string-methods/script.js
@@ -0,0 +1,77 @@
+/*
+charAt()
+concat()
+indexof()
+lastIndexOf()
+toUpperCase()
+toLowerCase()
+trim()
+slice()
+substring()
+replace()
+split()
+valueOf()
+startsWith()
+endsWith()
+*/
+const text = "hello world";
+
+// charAt METODU
+// const char=text.charAt(5);
+// console.log(char);
+
+
+//concat metodu
+// const word_1 = "hello";
+// const word_2="world";
+// const concat_word=word_1.concat(word_2);
+// console.log(concat_word);
+
+//indexof metodu
+// const index=text.indexOf("e");
+// console.log(index);
+
+//lastIndexOf metodu
+// const index=text.lastIndexOf("e");
+// console.log(index);
+
+//toUpperCase()
+// const up=text.toUpperCase();
+// console.log(up);
+
+//toLowerCase()
+//  const low=text.toLowerCase();
+//  console.log(low);
+
+//trim() metodu
+// const clean=text.trim();
+// console.log(clean);
+
+//slice() metodu
+// const cut=text.slice(5,11);
+// console.log(cut);
+
+//substring() metodu
+// const sub=text.substring(0,4);
+// console.log(sub);
+
+//replace() metodu
+// const rep=text.replace("world","hilal");
+// console.log(rep);
+
+//split() metodu
+// const spl=text.split("o",2);
+// console.log(spl);
+
+
+// valueOf() metodu
+// const value=text.valueOf("ad");
+// console.log(value);
+
+// startsWith() metodu
+// const start=text.startsWith("h");
+// console.log(start);
+
+// endsWith() metodu
+const end=text.endsWith("d");
+console.log(end);
